59423ab945ce4c837b4f16160889a5bef25eef60
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2020-01-15 20:10:33 +0000

    inital integration tests passed

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,34 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+include_recipe 'apt'
+
+execute 'mongodb_apt_key' do
+  command 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6'
+  action :run
+end
+
+execute 'mongodb_sourcelist' do
+  command "echo 'deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse' | sudo tee "
+  action :run
+end
+
+execute 'apt-update' do
+  command 'sudo apt-get update -y'
+  action :run
+end
+
+execute 'mongodb_install' do
+  command 'sudo apt-get install -y mongodb-org'
+  action :run
+end
+
+execute 'restart_mongod' do
+  command 'sudo systemctl restart mongod'
+  action :run
+end
+
+execute 'enable_mongod'do
+  command 'sudo systemctl enable mongod'
+  action :run
+end
